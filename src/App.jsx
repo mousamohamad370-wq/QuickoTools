38fd6e1691064aaa99c1b2bca5e493e1745527b3
Mohamad <mousamohamad370@gmail.com>
2026-03-07 14:01:57 +0200

    Add Character Counter tool and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import WordCounter from "./tools/WordCounter";
+import CharacterCounter from "./tools/CharacterCounter";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/word-counter" element={<WordCounter />} />
+       <Route path="/character-counter" element={<CharacterCounter />} />
     </Routes>
   );
 }
